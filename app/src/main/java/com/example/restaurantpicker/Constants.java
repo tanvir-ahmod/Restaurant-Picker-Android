b5cf39853e40bf572a1e7281daefa068120a286c
shoukhin93 <shoukhin1993@gmail.com>
2018-05-27 23:15:47 +0600

    User login complete

--- app/src/main/java/com/example/restaurantpicker/Constants.java
+++ app/src/main/java/com/example/restaurantpicker/Constants.java
@@ -7,8 +7,9 @@ package com.example.restaurantpicker;
 public class Constants {
 
     public static final String USER_REGISTRATION_URL = "http://192.168.161.1/Restaurant%20Picker/user_registration.php";
+    public static final String USER_LOGIN_URL = "http://192.168.161.1/Restaurant%20Picker/user_login.php";
+
+
     public static final String REQUEST_TAG = "com.example.restaurantpicker";
-
-
     public static final String LOGTAG = "logtag";
 }
---
+++ app/src/main/java/com/example/restaurantpicker/Login.java
@@ -0,0 +1,114 @@
+package com.example.restaurantpicker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends AppCompatActivity {
+
+    Button loginButton;
+    EditText editTextEmail, editTextPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initialize();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                userLogin();
+
+            }
+        });
+
+    }
+
+    private void userLogin() {
+        //first getting the values
+        final String tempEmail = editTextEmail.getText().toString();
+        final String password = editTextPassword.getText().toString();
+
+        //validating inputs
+        if (TextUtils.isEmpty(tempEmail)) {
+            editTextEmail.setError("Please enter your email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(tempEmail).matches()) {
+            editTextEmail.setError("Enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Please enter your password");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        //if everything is fine
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.USER_LOGIN_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //converting response to json object
+                            JSONObject obj = new JSONObject(response);
+
+                            //if no error in response
+                            if (!obj.getBoolean("error")) {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", tempEmail);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest, Constants.REQUEST_TAG);
+    }
+
+
+    private void initialize() {
+        loginButton = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+    }
+}
